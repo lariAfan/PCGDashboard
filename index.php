2f89e1f4c1be2e608d01e1bf4a2067b4587dbff4
Larisse Ormenese Garuti <larisse.garuti@ima.sp.gov.br>
2022-04-01 08:13:17 -0300

    alteração na semanal

--- index.php
+++ index.php
@@ -201,7 +201,7 @@
     <script>
         var pokemonAnterior = "";
         var spawn = "";
-        var tipoMissaoSemanal = "Fairy";        
+        var tipoMissaoSemanal = "Psychic";        
         const tiposPokemon = ['Normal','Fire', 'Water', 'Grass', 
         'Electric', 'Ice', 'Fighting', 'Poison', 'Ground', 
         'Flying', 'Psychic', 'Bug', 'Rock', 'Ghost', 'Dragon',
